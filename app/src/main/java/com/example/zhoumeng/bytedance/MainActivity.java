d0c8989b3f8d927553b3d62348f7dd3c0a140a43
C01DZin <732345308@qq.com>
2019-07-01 11:35:55 +0800

    增加activity测试

--- app/src/main/java/com/example/zhoumeng/bytedance/MainActivity.java
+++ app/src/main/java/com/example/zhoumeng/bytedance/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.zhoumeng.bytedance;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,18 +14,54 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTxtv;
     private String[] str = {"I'm back!","I've changed!"};
     private int index = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mBtn = findViewById(R.id.button);
         mTxtv = findViewById(R.id.textView);
+        System.out.println("on create");
+
         mBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 mTxtv.setText(str[(index++)%2]);
+                finish();
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
 
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("on destroy");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("on stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("on pause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        System.out.println("on restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("on resume");
     }
 }
